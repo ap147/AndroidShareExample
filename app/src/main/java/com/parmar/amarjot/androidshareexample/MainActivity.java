4405011266969564501ff1f7668017dfe6f38e52
Amarjot Parmar <amarjotparmar098@gmail.com>
2018-09-28 15:30:59 +1200

    Share button added

--- app/src/main/java/com/parmar/amarjot/androidshareexample/MainActivity.java
+++ app/src/main/java/com/parmar/amarjot/androidshareexample/MainActivity.java
@@ -1,7 +1,10 @@
 package com.parmar.amarjot.androidshareexample;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupShareButton();
+    }
+
+    private void setupShareButton() {
+
+        Button share = findViewById(R.id.buttonShare);
+
+        share.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                sendIntent.setType("text/plain");
+                String shareBody = "Body text goes here";
+                sendIntent.putExtra(Intent.EXTRA_TEXT, shareBody);
+
+                startActivity(Intent.createChooser(sendIntent, "Share using"));
+            }
+        });
     }
 }
